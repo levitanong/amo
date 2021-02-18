6366eaa1cbdbaad81eb9f7dcedb3138109713d3a
Levi Tan Ong <levi.ong@gmail.com>
2021-02-18 14:04:48 +0800

    update middleware to use new arg types

--- core/src/main/amo/middleware.cljs
+++ core/src/main/amo/middleware.cljs
@@ -4,7 +4,7 @@
 
 (defn wrap-map-handler
   [handler handler-map]
-  (fn [app effect-id effect-data]
+  (fn [{:keys [app]} effect-id effect-data]
     (if-let [map-handler (get handler-map effect-id)]
       (map-handler app effect-id effect-data)
       (handler app effect-id effect-data))))
@@ -13,7 +13,7 @@
   "If the output of handler is a channel, take from it.
     Regardless, pass this value to handle-chan"
   [handler handle-chan]
-  (fn [app effect-id effect-data]
+  (fn [{:keys [app]} effect-id effect-data]
     (let [maybe-tx-chan (handler app effect-id effect-data)]
       (if (and maybe-tx-chan (satisfies? ReadPort maybe-tx-chan))
         (go (handle-chan app effect-id effect-data (<! maybe-tx-chan)))
